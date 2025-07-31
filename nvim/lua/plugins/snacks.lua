65f4bca389a78c5b6d129d928b955f81625df783
eeldarrb <167944058+eeldarrb@users.noreply.github.com>
2025-07-31 00:20:08 -0400

    change dashboard screen

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,6 +1,45 @@
 return {
   "folke/snacks.nvim",
   opts = {
+    dashboard = {
+      preset = {
+        header = [[         
+ _  _  _  _  ____  __  __ 
+( \( )( \/ )(_  _)(  \/  )
+ )  (  \  /  _)(_  )    ( 
+(_)\_)  \/  (____)(_/\/\_)
+]],
+        keys = {
+          {
+            key = "l",
+            desc = "Settings",
+            action = ":Lazy",
+            enabled = package.loaded.lazy ~= nil,
+          },
+          {
+            key = "x",
+            desc = "Extras",
+            action = ":LazyExtras",
+            enabled = package.loaded.lazy ~= nil,
+          },
+          {
+            key = "q",
+            desc = "Quit",
+            action = ":qa",
+            hidden = true,
+          },
+        },
+      },
+      sections = {
+        { section = "header" },
+        {
+          pane = 2,
+          section = "keys",
+          align = "left",
+          padding = { 0, 3 },
+        },
+      },
+    },
     picker = {
       sources = {
         files = {
@@ -22,6 +61,7 @@ return {
         ".git",
         "node_modules",
         "venv",
+        ".ruff_cache",
         "__pycache__",
       },
     },
