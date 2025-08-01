b047b0d92eb72003845cb3a006940872f242039e
eeldarrb <167944058+eeldarrb@users.noreply.github.com>
2025-07-31 22:21:00 -0400

    change nvim theme to onedark

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,23 @@
 return {
-  { "rebelot/kanagawa.nvim" },
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000,
+    config = function()
+      require("onedark").setup({
+        style = "darker",
+      })
+      -- Enable theme
+      require("onedark").load()
+    end,
+  },
+  -- { "rebelot/kanagawa.nvim" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa",
+      colorscheme = "onedark",
     },
-  }, -- Disable tokyonight
+  },
+  -- Disable tokyonight
   { "folke/tokyonight.nvim", enabled = false },
   -- Disable catppuccin
   { "catppuccin/nvim", enabled = false },
